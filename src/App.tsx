7e0b8b9e001045160eb18ba5db2868da530899a3
u7chan <34462401+u7chan@users.noreply.github.com>
2024-09-30 23:36:08 +0900

    feat: automatic tailwind-css class sorting with biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  return (
+    <div className='flex h-screen justify-center'>
+      <div>
+        <h1 className='mt-2 mb-2 font-bold text-2xl underline'>Hello world!</h1>
+        <button
+          type='button'
+          className='inline-flex h-12 items-center justify-center rounded-md bg-neutral-950 px-6 font-medium text-neutral-50 transition active:scale-110'
+        >
+          Click me
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default App
