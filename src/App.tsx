e143cbb0449f7384369b8057bff0d6e1d0bbdfed
u7chan <34462401+u7chan@users.noreply.github.com>
2024-10-01 01:12:47 +0900

    fix: layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 function App() {
   return (
-    <div className='flex h-screen justify-center'>
-      <div>
-        <h1 className='mt-2 mb-2 font-bold text-2xl underline'>Hello world!</h1>
-        <button
-          type='button'
-          className='inline-flex h-12 items-center justify-center rounded-md bg-neutral-950 px-6 font-medium text-neutral-50 transition active:scale-110'
-        >
-          Click me
-        </button>
+    <div className='container mx-auto px-4'>
+      <div className='mt-2 flex justify-center'>
+        <img src='./logo.png' alt='logo' />
+      </div>
+      <div className='mt-2 flex justify-center'>
+        <div>KALEIDXSCOPE -青春エリア-</div>
+      </div>
+      <div className='mt-4 flex gap-3'>
+        <img src='./dummy.png' alt='' className='size-[80px] rounded-md border border-gray-500' />
+        <div className='flex-1 '>
+          <div className='line-clamp-2 h-[52px] '>
+            プリズム△▽リズムぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけ
+          </div>
+          <div className='flex h-[32px] items-center text-sm'>maimai でらっくす PRiSM</div>
+        </div>
+        <div className='flex w-[42px] flex-col justify-center gap-2'>
+          <div className='flex h-7 justify-center border-2 border-[#9F51DC] bg-[#9F51DC] text-white'>
+            12+
+          </div>
+          <div className='flex h-7 justify-center border-2 border-[#9F51DC] bg-white text-[#9F51DC]'>
+            13+
+          </div>
+        </div>
+        <div className='flex w-[48px] items-center justify-center'>
+          <input
+            id='default-checkbox'
+            type='checkbox'
+            value=''
+            className='h-4 w-4 scale-125 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600'
+          />
+        </div>
       </div>
     </div>
   )
