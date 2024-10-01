98fc0b8a93ee539d7357fd98b12676be99950ffb
u7chan <34462401+u7chan@users.noreply.github.com>
2024-10-01 23:16:45 +0900

    fix: layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Checkbox } from '@material-tailwind/react'
+
 function App() {
   return (
     <div className='container mx-auto px-4'>
@@ -21,20 +23,15 @@ function App() {
           <div className='flex h-[32px] items-center text-sm'>maimai でらっくす PRiSM</div>
         </div>
         <div className='flex w-[42px] flex-col justify-center gap-2'>
-          <div className='flex h-7 justify-center border-2 border-[#9F51DC] bg-[#9F51DC] text-white'>
+          <div className='flex h-6 items-center justify-center border-2 border-[#9F51DC] bg-[#9F51DC] font-bold text-sm text-white'>
             12+
           </div>
-          <div className='flex h-7 justify-center border-2 border-[#9F51DC] bg-white text-[#9F51DC]'>
+          <div className='flex h-6 items-center justify-center border-2 border-[#9F51DC] bg-white font-bold text-[#9F51DC] text-sm'>
             13+
           </div>
         </div>
         <div className='flex w-[48px] items-center justify-center'>
-          <input
-            id='default-checkbox'
-            type='checkbox'
-            value=''
-            className='h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600'
-          />
+          <Checkbox color='green' />
         </div>
       </div>
       <div className='my-2 h-px border-1 bg-gray-200' />
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+declare module 'react' {
+  interface HTMLAttributes {
+    placeholder?: string
+    onPointerEnterCapture?: React.PointerEventHandler
+    onPointerLeaveCapture?: React.PointerEventHandler
+  }
+}
